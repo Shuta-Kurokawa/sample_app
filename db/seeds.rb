348208784ebb474d796d9b6af3f834869e3eedc7
Shuta Kurokawa <maplesunny36@gmail.com>
2019-07-11 16:58:17 +0700

    Add follow unfollow function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 User.create!(name: "Example User",
-            email: "example@example.com",
-            password: "foobar",
-            password_confirmation: "foobar", admin: true)
+             email: "example@example.com",
+             password: "foobar",
+             password_confirmation: "foobar", admin: true)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@example.com"
   password = "password"
-User.create!(name: name,
-            email: email,
-            password: password,
-            password_confirmation: password)
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
 end
 
 users = User.order(:created_at).take(6)
@@ -18,3 +18,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
